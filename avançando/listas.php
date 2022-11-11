f636dffcaa20ba2bb0b97dabc921204e9068640c
Renatocoo <sdacostarenato@gmail.com>
2022-11-11 13:22:32 -0300

    add aula foreach e array associativo

--- avançando/listas.php
+++ avançando/listas.php
@@ -1,6 +1,9 @@
 <?php
 
 $idadeList = [21, 23, 19, 25, 30, 41, 18];
-$umaIdade = $idadeList[4];
 
-echo $umaIdade;
+$idadeList[] = 20;
+
+foreach ($idadeList as $idade) {
+    echo $idade . PHP_EOL;
+}
