1fbc2a2fd6ce4bf7700545d6e3ad67d1632fbfb3
renatocoo <sdacostarenato@gmail.com>
2023-01-24 17:26:47 -0300

    terminando php arrays e strings

--- avançando/banco.php
+++ avançando/banco.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'funcoes.php';
+
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Maria',
@@ -14,32 +16,6 @@ $contasCorrentes = [
         'saldo' => 100
     ]
 ];
-
-function sacar(array $conta, float $valorASacar): array
-{
-    if ($valorASacar > $conta['saldo']) {
-        exibeMensagem("Você não tem saldo suficiente");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-
-    return $conta;
-}
-
-function exibeMensagem(string $mensagem)
-{
-    echo $mensagem . PHP_EOL;
-}
-
-function depositar(array $conta, float $valorADepositar): array
-{
-    if ($valorADepositar > 0) {
-        $conta['saldo'] += $valorADepositar;
-    } else {
-        exibeMensagem("Depositos precisam ser positivos");
-    }
-    return $conta;
-}
 
 $contasCorrentes['123.456.789-10'] = sacar(
     $contasCorrentes['123.456.789-10'],
@@ -56,10 +32,13 @@ $contasCorrentes['123.256.789-12'] = depositar(
     900
 );
 
+unset($contasCorrentes['123.456.689-11']);
+
+titularComLetrasMaiusculas($contasCorrentes['123.256.789-12']);
+
 foreach ($contasCorrentes as $cpf => $conta) {
-    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+    ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    exibeMensagem(
+        "$cpf $titular $saldo"
+    );
 }
-
-
-
-
--- avançando/listas.php
+++ avançando/listas.php
@@ -2,6 +2,8 @@
 
 $idadeList = [21, 23, 19, 25, 30, 41, 18];
 
+list($idadeVinicius, $idadeJoao, $idadeMaria) = $idadeList;
+
 $idadeList[] = 20;
 
 foreach ($idadeList as $idade) {
